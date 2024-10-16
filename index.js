a24d7c3fd753b3344c289c0117a25a695f124de4
nortveeb2408 <conv.19aivfiiyfe2h7@fleep.io>
2024-10-16 09:10:02 +0300

    leong 04

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ejs = require('ejs')
 const path = require("path")
 
 const app = express()
-app.use(express.json())
+app.use(express.urlencoded())
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -15,14 +15,14 @@ const matk1 = {
     nimetus: "Sügismatk Kõrvemaal",
     pildiUrl: "/assets/maed.png",
     kirjeldus: "Lähme ja oleme kolm päeva looduses",
-    osalejad: ["mati@matkaja.ee", "kati@matkaja.ee"]
+    osalejad: []
 }
 
 const matk2 = {
     nimetus: "Süstamatk Hiiumaal",
     pildiUrl: "/assets/maed.png",
     kirjeldus: "Lähme ja oleme kolm päeva vee peal",
-    osalejad: ["mati@matkaja.ee", "kati@matkaja.ee", "uudo@ryhkija.ee"]
+    osalejad: []
 }
 
 const matkad = [
@@ -32,9 +32,26 @@ const matkad = [
         nimetus: "Mägimatk Otepääl",
         pildiUrl: "/assets/maed.png",
         kirjeldus: "Lähme ja oleme kolm päeva mägedes",
-        osalejad: ["uudo@ryhkija.ee"]
+        osalejad: []
     }
 ]
+
+const s6nunid = []
+
+function registreeruMatkale(matkaIndex, nimi, email) {
+    if (matkaIndex > matkad.length) {
+        console.log("Vale matka indeks")
+        return
+    }
+    const matk = matkad[matkaIndex]
+    const uusMatkaja = {
+        nimi: nimi,
+        email: email,
+        registreerumiseAeg: new Date()
+    }
+    matk.osalejad.push(uusMatkaja)
+    console.log(matkad)
+}
 
 
 app.get('/test', (req, res) => {res.end('kõik töötab!')})
@@ -43,7 +60,29 @@ app.use('/', express.static("public"))
 app.get('/', (req, res)=> { res.render("esileht", {matkad: matkad}) })
 app.get('/matk/:matkId', (req, res) => {
     const matkaIndex = req.params.matkId
-    res.render("matk", { matk: matkad[matkaIndex] }) 
+    res.render("matk", { matk: matkad[matkaIndex], id: matkaIndex }) 
 })
+
+app.get('/registreerumine', (req, res) => {
+   registreeruMatkale(req.query.matkaIndex,req.query.nimi,req.query.email)
+   res.render('reg_kinnitus', {matk: matkad[req.query.matkaIndex], matkaja: req.query.nimi})
+})
+
+app.post('/registreerumine', (req, res) => {
+    console.log(req.body)
+    registreeruMatkale(req.body.matkaIndex, req.body.nimi, req.body.email)
+    res.render('reg_kinnitus', {matk: matkad[req.body.matkaIndex], matkaja: req.body.nimi})
+ })
+
+ app.post('/sonum', (req, res) => {
+    //kontrolli console.log abil, kas andmed jõuavad kohale
+    console.log(req.body)
+    // tee funktsioon mis salvestab saadud andmed sõnumite massiivi
+    // s6numid.push(uusSonum)
+
+    //renderda mall sõnumi kättesaamise kohta
+
+    console.log(s6numid)
+ })
 
 app.listen(PORT)
